268002238b8e86454bf414994dd794f44ceeea86
Serdar Doğan <serdar.dogaan@gmail.com>
2020-10-06 00:13:13 +0300

    Camera Follow script tamamlandı.

---
+++ HelixJumpCopy/Assets/Scripts/BallParentScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallParentScript : MonoBehaviour {
+
+    [SerializeField]
+    Transform ballTransform;
+
+    [HideInInspector]
+    public float lowestYPos;
+
+    private void Start() {
+        lowestYPos = ballTransform.position.y;
+    }
+
+    private void Update() {
+        if (ballTransform.position.y < lowestYPos) {
+            lowestYPos = ballTransform.position.y;
+        }
+    }
+}
---
+++ HelixJumpCopy/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    [SerializeField]
+    BallParentScript target;
+
+    [SerializeField]
+    Vector3 offset;
+
+    Transform targetTransform;
+
+    private void Start() {
+        targetTransform = target.transform;
+    }
+
+    private void FixedUpdate() {
+        Vector3 desiredPosition = new Vector3(targetTransform.position.x + offset.x, target.lowestYPos + offset.y, targetTransform.position.z + offset.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, 0.125f);
+        transform.position = smoothedPosition;
+    }
+
+}
