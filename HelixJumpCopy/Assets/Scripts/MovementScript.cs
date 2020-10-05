e44b583e51031dafc895f251a862dff9942b12fa
Serdar Doğan <serdar.dogaan@gmail.com>
2020-10-06 00:04:27 +0300

    Sağ sol hareket kontrolü

---
+++ HelixJumpCopy/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    private void Update() {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal"));
+    }
+
+}
