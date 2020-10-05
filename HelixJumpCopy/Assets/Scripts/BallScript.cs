dc9c76fa41e0cba31577419b7c74e2c91fc91240
Serdar Doğan <serdar.dogaan@gmail.com>
2020-10-05 23:27:28 +0300

    Level Design ve Objelerin temel halleri.
    -Materialler hazırlandı.
    -Prefablar hazırlandı.
    -Scriptler oluşturuldu.

---
+++ HelixJumpCopy/Assets/Scripts/BallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    Rigidbody rb;
+
+    [SerializeField]
+    GameObject splashObject;
+
+    [SerializeField]
+    float addForceY;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "RingPiece") {
+            Debug.Log("RingPiece");
+            rb.AddForce(new Vector3(0, addForceY, 0), ForceMode.Force);
+            Instantiate(splashObject, collision.transform.position, Quaternion.identity);
+        }
+    }
+}
