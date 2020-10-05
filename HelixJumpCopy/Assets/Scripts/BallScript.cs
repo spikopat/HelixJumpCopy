7585a49df650995c0cf325b164ac17acb3884aca
Serdar Doğan <serdar.dogaan@gmail.com>
2020-10-05 23:35:21 +0300

    Topun temas tetiği noktaya splash effect bırakması.

--- HelixJumpCopy/Assets/Scripts/BallScript.cs
+++ HelixJumpCopy/Assets/Scripts/BallScript.cs
@@ -18,9 +18,11 @@ public class BallScript : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "RingPiece") {
-            Debug.Log("RingPiece");
+
+            Vector3 contactPoint = collision.contacts[0].point;
+            Instantiate(splashObject, new Vector3(contactPoint.x, contactPoint.y + 0.1f, contactPoint.z), Quaternion.Euler(90, 0, 0));
+
             rb.AddForce(new Vector3(0, addForceY, 0), ForceMode.Force);
-            Instantiate(splashObject, collision.transform.position, Quaternion.identity);
         }
     }
 }
---
+++ HelixJumpCopy/Assets/Scripts/SplashEffectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashEffectScript : MonoBehaviour {
+
+    [SerializeField]
+    float timeAfterDestroy;
+
+    private void Start() {
+        Destroy(gameObject, timeAfterDestroy);
+    }
+
+}
